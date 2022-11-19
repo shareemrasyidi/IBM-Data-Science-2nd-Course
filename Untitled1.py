3b4df7c3d87a52220d860c2b5383d61b88751c58
shareemrasyidi <110925465+shareemrasyidi@users.noreply.github.com>
2022-11-19 16:42:24 +0700

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Shareem Rasyidi**
+# 
+# Data Scientist
+
+# *I am interested in data science because I have been really enjoying working with some data and getting the insight from it to learn something new. When i tried to learn data science I also get really hooked to it and decides to pursue my career more to this field. Additionally, the prospect of this field is really promising in the future so that is also a plus*
+
+# ### The following line will try to demonstrate Python ability to generate an output, for the sake of simplicity, I will only use simple mathematic operation like 1+1
+
+# In[5]:
+
+
+1+1
+
+
+# ***
+# - Bullet List
+# 1. This is numbered list
+# 2. Testing further numbered list
